1cdacfc8be972c45d9dfe0a78bc299065becf381
Javiera Quilodran <jvcatalina.iccp@gmail.com>
2023-05-03 11:49:16 -0400

    ok

---
+++ fundamentals/fundamentals/HolaMundo.py
@@ -0,0 +1,18 @@
+print(Hola,mundo)
+
+nombre = "javiera"
+
+print("Hola", nombre)
+
+print("Hola" + nombre)
+
+num = 6
+
+print("!Hola", num)
+
+print("!Hola"+ num)
+
+comida1 = "ensalada"
+comida2 = "arroz"
+print("Amo comer", comida1, comida2)
+print(f"Amo comer", comida1, comida2)
